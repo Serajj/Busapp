2869f59162f9f01b1872ab99df4c58478ea85cc7
Serajj <rajalam2372@gmail.com>
2021-04-10 17:59:20 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+
+    res.json({
+        "Message": "Server is running perfectly"
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Server Running");
+})
